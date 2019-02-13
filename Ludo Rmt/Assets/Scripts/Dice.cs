ac6efba5fe9e76cba02db670752517768f6138d3
JokicNikola <jokic1916@fon.bg.ac.rs>
2019-02-13 03:10:36 +0100

    Tranzicija

--- Ludo Rmt/Assets/Scripts/Dice.cs
+++ Ludo Rmt/Assets/Scripts/Dice.cs
@@ -3,15 +3,18 @@ using UnityEngine;
 
 public class Dice : MonoBehaviour {
 
-    public GameObject[] waypoints= new GameObject[60];
+   
     private GameObject pawn;
     private GameObject check;
     int index = 2;
     private Sprite[] diceSides;
     private SpriteRenderer rend;
     public bool out_=false;
+    int randomDiceSide;
+    int randomDiceSide1;
 
-	private void Start () {
+
+    private void Start () {
         rend = GetComponent<SpriteRenderer>();
         pawn = GameObject.Find("pawn");
         diceSides = Resources.LoadAll<Sprite>("DiceSides/");
@@ -22,37 +25,64 @@ public class Dice : MonoBehaviour {
         StartCoroutine("RollTheDice");
     }
 
+    private void Update()
+    {
+       
+        if (!out_ && (randomDiceSide1 + 1) == 6){
+            check = GameObject.Find("Waypoint (2)");
+            pawn.transform.position = check.transform.position;
+           // out_ = true;
+        }
+        else
+        {
+
+            if ( out_)
+            {
+                // index = index + randomDiceSide + 1;
+
+                check = GameObject.Find("Waypoint (" + index + ")");
+                pawn.transform.position = Vector3.MoveTowards(pawn.transform.position, check.transform.position, 3f * Time.deltaTime);
+            }
+
+        }
+    }
+
     private IEnumerator RollTheDice()
     {
-        int randomDiceSide = 0;
-        
+        randomDiceSide = 0;
+        randomDiceSide1 = 0;
+
+
 
         for (int i = 0; i <= 20; i++)
         {
             randomDiceSide = Random.Range(0, 6);
             rend.sprite = diceSides[randomDiceSide];
             yield return new WaitForSeconds(0.05f);
-            Debug.Log(randomDiceSide);
+            
         }
+        randomDiceSide1 = Random.Range(0, 6);
+        rend.sprite = diceSides[randomDiceSide1];
+        
 
-        if (!out_ && (randomDiceSide + 1) == 6)
+        if (!out_ && (randomDiceSide1 + 1) == 6)
         {
-            check = GameObject.Find("Waypoint (2)");
-            pawn.transform.position = check.transform.position;
+            //check = GameObject.Find("Waypoint (2)");
+            //pawn.transform.position = check.transform.position;
             out_ = true;
         }
         else
         {
 
-
-            if (index + randomDiceSide + 1 <= 52 && out_)
+            if ((index + randomDiceSide1 + 1) < 53 && out_)
             {
-                index = index + randomDiceSide + 1;
-                check = GameObject.Find("Waypoint (" + index + ")");
-                pawn.transform.position = check.transform.position;
+                index = index + randomDiceSide1 + 1;
+                //check = GameObject.Find("Waypoint (" + index + ")");
+                //pawn.transform.position = Vector3.MoveTowards(pawn.transform.position, check.transform.position, 1f*Time.deltaTime);
             }
 
         }
-       
+        Debug.Log(index+" ovo je klik");
+
     }
 }
